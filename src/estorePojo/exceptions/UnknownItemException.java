6592c8d7d0c35c8b0a23f11adf470788da86fcdf
Tommy Bezoky <bezokytommy@hotmail.com>
2023-01-20 14:57:40 +0100

    Add services and data packages

--- src/estorePojo/exceptions/UnknownItemException.java
+++ src/estorePojo/exceptions/UnknownItemException.java
@@ -3,15 +3,15 @@ package estorePojo.exceptions;
 public class UnknownItemException extends Exception {
 
     private static final long serialVersionUID = -1585427033037517192L;
-    
+
     private Object item;
 
-    public UnknownItemException( String msg ) {
+    public UnknownItemException(String msg) {
         super(msg);
     }
-    
-	@Override
-	public String getMessage() {
-        return "Item "+item+" is not an item delivered by this provider.";
+
+    @Override
+    public String getMessage() {
+        return "Item " + item + " is not an item delivered by this provider.";
     }
 }
